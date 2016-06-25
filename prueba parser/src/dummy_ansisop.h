4c9b1d0bb0d01702ff77ad5d00b0c3f565de1a15
Julian Alvarez <alvarezjulianignacio@gmail.com>
2016-06-25 15:32:33 -0300

    pequeña prueba del parser con estructuras

--- prueba parser/src/dummy_ansisop.h
+++ prueba parser/src/dummy_ansisop.h
@@ -3,14 +3,21 @@
 #define DUMMY_ANSISOP_H_
 
 	#include <parser/parser.h>
-
 	#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
 
-	t_puntero dummy_definirVariable(t_nombre_variable variable);
+	typedef struct {
+	int posicion ;
+	char size;
+	}t_posicion;
 
-	t_puntero dummy_obtenerPosicionVariable(t_nombre_variable variable);
-	t_valor_variable dummy_dereferenciar(t_puntero puntero);
-	void dummy_asignar(t_puntero puntero, t_valor_variable variable);
+	t_posicion * dummy_definirVariable(t_nombre_variable variable);
+
+	t_posicion * dummy_obtenerPosicionVariable(t_nombre_variable variable);
+	t_valor_variable dummy_dereferenciar(t_posicion * puntero);
+	void dummy_asignar(t_posicion * puntero, t_valor_variable variable);
 
 	void dummy_imprimir(t_valor_variable valor);
 	void dummy_imprimirTexto(char* texto);
