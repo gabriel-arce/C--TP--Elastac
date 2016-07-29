586106fdf01cafa85eca9815df1cb304cb8bfa08
Julian Alvarez <alvarezjulianignacio@gmail.com>
2016-07-28 23:31:06 -0300

    commit para pull

--- cpu/src/CPU.c
+++ cpu/src/CPU.c
@@ -599,7 +599,6 @@ char* obtenerInstruccion(t_indice_de_codigo * instruccionACorrer){
 			offset = 0;
 			aux -= loQueGuardo;
 
-			printf("instruccion: %s", instruccion); //TODO prueba
 		}
 
 		string_append(&instruccion, leerBytesDeInstruccion(pagina, offset, aux));
